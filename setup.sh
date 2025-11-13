87a10c86cba62ece4ba2b17e6cee2d1704cc7dc8
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-13 12:15:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-bash run.sh
+#1. get pwd - should be /root/block-cpanel.php
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+#2. run.sh to add it once
+bash "$SCRIPT_DIR/run.sh"
+
+#3. setup.sh to re-add it after every cpanel update
 line_to_add="cd /root/block-cpanel.php/ && git pull ; bash /root/block-cpanel.php/setup.sh #https://github.com/stefanpejcic/block-cpanel.php"
-
 if ! grep -qF "$line_to_add" /scripts/postupcp; then
+  echo "Adding to /scripts/postupcp file.."
   echo "$line_to_add" >> /scripts/postupcp
+else
+  echo "Already added to /scripts/postupcp"
 fi
+
+echo "DONE"
