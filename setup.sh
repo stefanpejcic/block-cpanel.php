bcaf458cae94b2c75336c43a398f0f344c8233c8
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-11-13 11:59:36 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+FILE="/usr/local/cpanel/php/cpanel.php"
+
+read -r -d '' BLOCK <<'EOF'
+if (!isset($_ENV['CPANEL']) && !isset($_SERVER['REMOTE_USER'])) {
+    header('HTTP/1.1 403 Forbidden');
+    exit('Message support@your-domain.com if you need API access!');
+}
+EOF
+
+if [[ ! -f "$FILE" ]]; then
+  echo "Error: $FILE not found! - is this cpanel?"
+  exit 1
+fi
+
+if grep -Fq "your-domain.com" "$FILE"; then
+  echo "Already blocked in $FILE"
+else
+  tmpfile=$(mktemp)
+  awk -v block="$BLOCK" '
+    BEGIN { inserted=0 }
+    {
+      print $0
+      if (!inserted && $0 ~ /^<\?php/) {
+        print block "\n"
+        inserted=1
+      }
+    }
+  ' "$FILE" > "$tmpfile" && mv "$tmpfile" "$FILE"
+  echo "Added to $FILE"
+fi
+
